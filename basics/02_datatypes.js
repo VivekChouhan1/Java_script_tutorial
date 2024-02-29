2cd984fe73ac27746819a26b313e6862662d8086
VivekChouhan1 <118923330+VivekChouhan1@users.noreply.github.com>
2024-02-29 10:30:42 +0000

    day 01 learn

---
+++ basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all java code as newer version
+
+// alert("hello")   //we are using nodejs not browser thats why it not work here
+
+console.log(3+3); console.log("vivek");  //code readability should be high
+
+/* 
+NOTE:- 
+ECMA or MDN should used for standard's reading
+*/
+
+let name="vivii"
+let age=18
+let loggedgin=false;
+
+//NOTES:
+// 01. number =>2 to power of 53
+// 02. bigint =>used for very large number
+// 03. string =>" "
+// 04. boolean=> true or false
+// 05. null =>standalone value and it is object 
+// 06. undefined =>abhi value assign nahi hui hai and it is different from null
+// 07. symbol =>unique
+//object 
+
+console.log(typeof null)   //ouput = object
+console.log(typeof undefined)   //ouput = undefined (beacuse it is an type)
+
+
---
+++ basics/variable.js
@@ -0,0 +1,21 @@
+const accoundId=144553;    //const is an keyword and assigned memory
+let accountEmail="vivek2022@gmail.com";  
+var accountPassword="12235"
+accountCity="jaipur"
+let accountState;
+
+
+/* 
+NOTE:-
+prefer not to use "var" ,because of issue in block scope and functional scope */
+
+// accoundId=2  //this is not allowed because of const
+
+accountEmail="vivii2922@gmail.com"
+accountPassword="123333333"
+accountCity="indore"
+
+console.log(accoundId);
+
+console.table([accoundId,accountEmail,accountPassword,accountCity,accountState])  //used for get all data in table form
+
