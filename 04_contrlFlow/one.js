14e2dc584cbd688c835e49c1e332ba684957b3b8
VivekChouhan1 <118923330+VivekChouhan1@users.noreply.github.com>
2024-03-05 13:35:07 +0000

    contrl flow

---
+++ 04_contrlFlow/one.js
@@ -0,0 +1,11 @@
+//always should run on conditinal basis:
+// const isUserLogedIn=true;
+// if(isUserLogedIn){
+
+// }
+
+// const num=90;
+// if(num<100) console.log("test");
+// Operators: >,<,<=,>=,==,!=,===(check with type),!==(check with type)
+
+
